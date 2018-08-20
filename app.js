d5d38e4d55597905b118002773e354c46dfc2a95
TadejVatovani <vatko1995@gmail.com>
2018-08-21 01:01:19 +0200

    Setting up the basic server

--- app.js
+++ app.js
@@ -6,25 +6,14 @@
 
  /** External modules **/
  var express = require('express');
- var mongoose = require('mongoose');
- var bodyParser = require('body-parser');
- var cookieParser = require('cookie-parser');
 
 
  /** Internal modules **/
  var config = require('./private/config');
  var userController = require('./controllers/UserController');
 
- /** Database setup **/
- mongoose.connect(config.DB_PATH);
-
  /** Express setup **/
  var app = express();
-
- app.set('trust proxy',1) // trust first proxy
- app.set('json spaces',4);
- app.use(cookieParser());
- app.use(bodyParser.urlencoded({ extended: false}));
 
  /** Express routing **/
  app.use('/', userController);
